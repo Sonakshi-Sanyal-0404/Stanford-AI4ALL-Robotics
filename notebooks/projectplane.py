346f6115c43b7e21e6ce29f19e414237fdbb9ad1
claireliuunicorn <claireliuunicorn@gmail.com>
2025-06-30 15:31:22 -0400

    added non-biodegradable

--- notebooks/projectplane.py
+++ notebooks/projectplane.py
@@ -14,15 +14,29 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
 print("Loaded PyBullet plane.")
 
+#biodegradable
+
 banana = p.loadURDF("notebooks\ycb_assets/011_banana.urdf")
+
+
 apple = p.loadURDF("notebooks\ycb_assets/013_apple.urdf")
 box = p.loadURDF("notebooks\ycb_assets/003_cracker_box.urdf")
 strawberry = p.loadURDF("notebooks\ycb_assets/012_strawberry.urdf")
 pear = p.loadURDF("notebooks\ycb_assets/016_pear.urdf")
 
+#non biodegradable
+tennisball = p.loadURDF("notebooks\ycb_assets/056_tennis_ball.urdf")
+mug = p.loadURDF("notebooks\ycb_assets/025_mug.urdf")
+soccerball = p.loadURDF("notebooks\ycb_assets/053_mini_soccer_ball.urdf")
+airplane = p.loadURDF("notebooks\ycb_assets/072-a_toy_airplane.urdf")
+beachcleanser = p.loadURDF("notebooks\ycb_assets/021_bleach_cleanser.urdf")
 
 
-p.setGravity(0, 0, -50)
+
+
+
+
+p.setGravity(0, 0, -60)
 
 
 
