f7f85c08fd0743a91ceaade1ecb1394fd3cc7916
claireliuunicorn <claireliuunicorn@gmail.com>
2025-06-30 15:16:52 -0400

    new changes

--- notebooks/projectplane.py
+++ notebooks/projectplane.py
@@ -14,7 +14,6 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
 print("Loaded PyBullet plane.")
 
-<<<<<<< HEAD
 banana = p.loadURDF("notebooks\ycb_assets/011_banana.urdf")
 apple = p.loadURDF("notebooks\ycb_assets/013_apple.urdf")
 box = p.loadURDF("notebooks\ycb_assets/003_cracker_box.urdf")
@@ -23,17 +22,10 @@ pear = p.loadURDF("notebooks\ycb_assets/016_pear.urdf")
 
 
 
-p.setGravity(0, 0, -60)
+p.setGravity(0, 0, -50)
 
 
 
-
-
-
-
-=======
-crackerbox = p.loadURDF("ycb_assets/011_banana.urdf")
->>>>>>> ef34fec0bca3ea1a6580c849b396a4ed0087b076
 
 while True:
     p.stepSimulation()
