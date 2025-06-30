f852d7ab268bf0f35e5193d63c91f32fe164ac15
Claire Liu <claireliuunicorn@gmail.com>
2025-06-30 14:06:16 -0400

    empty plane

---
+++ notebooks/projectplane.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import pybullet as p
+import pybullet_data
+import matplotlib.pyplot as plt
+import time
+
+
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+planeId = p.loadURDF("plane.urdf")
+print("Loaded PyBullet plane.")
+
+crackebox = p.loadURDF("011_banana.urdf")
+
+while True:
+    p.stepSimulation()
+    time.sleep(0.01)
+p.disconnect()
+print("Simulation step completed and disconnected.")
